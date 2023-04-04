d13303352ab28705ffd99908ffa3206e5c222c06
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-04 20:22:48 +0300

    Меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,28 +1,28 @@
-
-const menuLinks = document.querySelectorAll('.menu_sub');
-console.log(menuLinks.length);
+const menuLinks = document.querySelectorAll(".menu__item");
+// console.log(menuLinks.length);
 console.log(menuLinks);
+
 menuLinks.forEach((link) => {
   link.onclick = function() {
-    console.log("onclick");
-    const menu = link.closest('menu');
-    const isActive = menu.classList.contains('menu_active');
-    const subMenu = link.querySelector('.menu_sub');
-    
+    // console.log("onclick");
+    const menu = link.closest(".menu");
+    const isActive = menu.classList.contains(".menu_active");
+    const subMenu = link.querySelector(".menu_sub");
+
     if (subMenu) {
-        console.log("subMenu");
-      subMenu.classList.add('.menu_active');
+      // console.log("subMenu = "+subMenu);
+      subMenu.classList.add("menu_active");
       return false;
     }
-    
+
     if (isActive) {
-        console.log("isActive");
-      menu.classList.remove('.menu_active');
+      // console.log("isActive");
+      menu.classList.remove("menu_active");
     } else {
-        iconsole.log("else");
-      const menus = document.querySelectorAll('menu');
-      menus.forEach((m) => m.classList.remove('.menu_active'));
-      menu.classList.add('menu_active');
+      // console.log("else");
+      const menus = document.querySelectorAll(".menu");
+      menus.forEach((m) => m.classList.remove(".menu_active"));
+      menu.classList.add(".menu_active");
     }
-  };
+   };
 });
