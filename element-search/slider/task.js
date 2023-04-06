e7719338d0b3e45d8dc936afecfeead0c129ec78
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-06 20:55:03 +0300

    ДЗ слайдер Готово

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -7,58 +7,56 @@ const slideWidth = sliderItems[0].clientWidth;
 let position = 0;
 let slideIndex = 0;
 
-// const buttonPrev = document.querySelector(".slider__arrow_prev");
-// buttonPrev.onclick = function () {
-//   const slider__item = document.querySelector(".slider__item");
-//   slider__item.classList.add("slider__item_active");
-// };
-
-// const buttonNext = document.querySelector(".slider__arrow_next");
-// buttonPrev.onclick = function () {
-//   const slider__item = document.querySelector(".slider__item");
-//   slider__item.classList.add("slider__item_active");
-// };
-
 function nextSlide() {
-  if (slideIndex > sliderItems.length -1) {
-    sliderItems[slideIndex].classList.remove("slider__item_active");
-    slideIndex = 0;
-  }
-  slideIndex++;
-  sliderItems[slideIndex-1].classList.remove("slider__item_active");
-  console.log("slideIndex = " + slideIndex);
-  sliderItems[slideIndex].classList.add("slider__item_active");
+    if (slideIndex < sliderItems.length -1 ){
+        slideIndex ++;
+    } else {
+        slideIndex = 0;
+        sliderItems[sliderItems.length -1].classList.remove("slider__item_active");
+        sliderItems[slideIndex].classList.add("slider__item_active");
+        updateDots();
+        return;
+    }
+  sliderItems[slideIndex-1].classList.remove("slider__item_active"); 
+  sliderItems[slideIndex].classList.add("slider__item_active");  
   updateDots();
 }
 
 function prevSlide() {
-  if (slideIndex <= 0) {
-    slideIndex = sliderItems.length -1;
-    // sliderItems[0].classList.remove("slider__item_active");
-    // if (slideIndex == 5) slideIndex=4;
-  }
-  console.log("slideIndex До = " + slideIndex);
-  sliderItems[slideIndex-1].classList.remove("slider__item_active");
-  
-  console.log("slideIndex = " + slideIndex);
-  sliderItems[slideIndex].classList.add("slider__item_active");
-  slideIndex--;
+    if (slideIndex <= 0 ){
+        slideIndex = sliderItems.length -1;
+        sliderItems[0].classList.remove("slider__item_active");
+        sliderItems[slideIndex].classList.add("slider__item_active");
+        updateDots();
+        return;
+    } else {
+        slideIndex --;
+    }
+    sliderItems[slideIndex+1].classList.remove("slider__item_active");
+    sliderItems[slideIndex].classList.add("slider__item_active");
+    // slideIndex--;
   updateDots();
 }
 
 function goToSlide(index) {
   slideIndex = index;
-  position = -(slideWidth * (slideIndex - 1));
-  sliderWrapper.style.transform = `translateX(${position}px)`;
-  updateDots();
+    sliderItems.forEach((Items, index) => {
+        if (index === slideIndex) {
+            Items.classList.add("slider__item_active");
+        } else {
+            Items.classList.remove("slider__item_active");
+        }
+        updateDots();
+    });
 }
+
 
 function updateDots() {
   sliderDots.forEach((dot, index) => {
-    if (index === slideIndex - 1) {
-      dot.classList.add("active");
+    if (index === slideIndex) {
+      dot.classList.add("slider__dot_active");
     } else {
-      dot.classList.remove("active");
+      dot.classList.remove("slider__dot_active");
     }
   });
 }
@@ -68,6 +66,6 @@ sliderLeftBtn.addEventListener("click", prevSlide);
 
 sliderDots.forEach((dot, index) => {
   dot.addEventListener("click", () => {
-    goToSlide(index + 1);
+    goToSlide(index);
   });
 });
