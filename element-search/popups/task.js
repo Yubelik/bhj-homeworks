d8e82710dea3b29a6f4c90934b217e7fb0fd9a45
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-11 15:07:08 +0300

    поправил задание menu

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,16 @@
 const modalMain = document.querySelector("#modal_main");
 modalMain.classList.add("modal_active");
 
-const closeModal = document.querySelector(".modal__close");
-closeModal.onclick = function() {
- modalMain.classList.remove("modal_active");
-}
+
 
 const show = document.querySelector(".show-success");
 show.onclick = function() {
     const modalSuccessShow = document.querySelector("#modal_success");
+    modalMain.classList.remove("modal_active");
     modalSuccessShow.classList.add("modal_active");
    }
 
-// const closeModal = document.querySelector(".modal__close");
-// closeModal.onclick = function() {
-//  modalMain.classList.remove("modal_active");
-// }
+document.querySelectorAll(".modal__close")
+  .forEach(el => el.addEventListener("click",  function(e){ 
+    this.parentNode.parentNode.classList.remove("modal_active"); 
+  }));
