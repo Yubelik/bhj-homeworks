0bf6f12333549e6f6fcab5e4dd8db78d1cb95d29
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-20 21:41:36 +0300

    Дз готово

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,15 +17,30 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
+    document.addEventListener('keydown', (event) => {
+      const enteredSymbol = event.key.toLowerCase();
+      const currentSymbol = this.currentSymbol.textContent.toLowerCase();
+      if (enteredSymbol === currentSymbol) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
+
+    // document.addEventListener('keyup', (event) => {
+    //     const enteredSymbol = event.key.toLowerCase();
+    //     const currentSymbol = this.currentSymbol.textContent.toLowerCase();
+    //     console.log("enteredSymbol = "+enteredSymbol);
+    //     console.log("currentSymbol = "+currentSymbol);
+    //     if (enteredSymbol === currentSymbol) {
+    //       this.success();
+    //     } else {
+    //       this.fail();
+    //     }
+    //   });
+    }
+    
+  
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
