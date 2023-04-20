3ea94aefbe7c527c149ca6330b3eb11aa01f6798
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-20 23:02:53 +0300

    Дз готово

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+const tabNavi = document.querySelectorAll('.tab');
+const tabContent = document.querySelectorAll('.tab__content');
+
+
+tabNavi.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      
+      tabNavi.forEach(t => t.classList.remove('tab_active'));
+      tabContent.forEach(c => c.classList.remove('tab__content_active'));
+  
+      
+      tab.classList.add('tab_active');
+      tabContent[index].classList.add('tab__content_active');
+    });
+  });
