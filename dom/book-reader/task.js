13b942b5d43cec4e09c53d99b317ceb13047b72b
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-21 22:48:43 +0300

    done

---
+++ dom/book-reader/task.js
@@ -0,0 +1,28 @@
+const controls = Array.from(document.querySelectorAll('.font-size'));
+const book = document.querySelector(".book");
+const sizeOfText = ["book_fs-small", " ", "book_fs-big"];
+
+function changeSize(i) {
+    controls.forEach(item => {
+        item.classList.remove('font-size_active');
+    });
+    
+    controls[i].classList.add("font-size_active");
+	let checkClass = sizeOfText[i];
+    book.classList.remove("book_fs-small");
+	book.classList.remove("book_fs-big");
+
+	if(i != 1) {
+        book.classList.add(checkClass);
+    }
+    
+	// return false
+}
+
+controls.forEach((element, index) => {
+    element.addEventListener('click', (event) => {
+    let i = controls.indexOf(element)
+    changeSize(i);
+    event.preventDefault(); 
+    });
+  });
