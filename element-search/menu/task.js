5653bb9a7ee262fe621c0de20ab355451f9a23f7
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-04-21 10:35:39 +0300

    Done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,16 +4,14 @@ const items = Array.from(document.querySelectorAll('.menu__item'));
 items.forEach(item => {
   const link = item.querySelector('.menu__link');
   link.addEventListener('click', (event) => {
-    
     const sublist = item.querySelector('.menu_sub');
-    console.log("sublist = "+sublist.innerHTML);
     if (sublist) {
       const activeSublist = document.querySelector('.menu_active');
-      event.preventDefault(); // Предотвращаем переход по ссылке
+      event.preventDefault(); 
       if (activeSublist && activeSublist !== sublist) {
-        activeSublist.classList.remove('menu_active'); // Закрываем предыдущее открытое меню
+        activeSublist.classList.remove('menu_active'); 
       }
-      sublist.classList.toggle('menu_active'); // Переключаем класс у вложенного меню
+      sublist.classList.toggle('menu_active'); 
     }
   });
 });
